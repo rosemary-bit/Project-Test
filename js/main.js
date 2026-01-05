6bcf942b33c627fcc4ced3e0a06d428490609815
vitalitytokyo <mahdiajjour2004@gmail.com>
2026-01-05 11:07:14 +0200

    .da.da

---
+++ js/main.js
@@ -0,0 +1,18 @@
+document.getElementById("colo").onclick = () => {
+    window.location.href= "Colosseum.html" ;
+};
+document.getElementById("baalbek").onclick = () => {
+    window.location.href= "Baalbek Temple.html" ;
+};
+document.getElementById("rock").onclick = () => {
+    window.location.href= "Raouché Rocks.html" ;
+};
+document.getElementById("eiffel").onclick = () => {
+    window.location.href= "Eiffel Tower.html" ;
+};
+document.getElementById("statue").onclick = () => {
+    window.location.href= "Statue of Liberty.html" ;
+};
+document.getElementById("tokyo").onclick = () => {
+    window.location.href= "Fushimi Inari Shrine.html" ;
+};
