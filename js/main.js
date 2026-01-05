890c51c70eaa916153a83640040e523b8d2e199f
vitalitytokyo <mahdiajjour2004@gmail.com>
2026-01-05 14:05:52 +0200

    pain

--- js/main.js
+++ js/main.js
@@ -1,5 +1,42 @@
 document.getElementById("highlight").style.backgroundColor = "rgb(99, 14, 14)";
 
+const form = document.querySelector("form");
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  const name = document.getElementById("name").value.trim();
+  const email = document.getElementById("gmail").value.trim();
+  const message = document.getElementById("message").value.trim();
+
+  if (name.length < 3) {
+    alert("Name is too small");
+    return;
+  }
+
+  if (!email.includes("@") || !email.includes(".")) {
+    alert("Invalid email");
+    return;
+  }
+
+  if (message.length < 10) {
+    alert("Message is too short");
+    return;
+  }
+  const subject = encodeURIComponent("Feedback received");
+const body = encodeURIComponent("Thank you for your feedback.");
+
+window.location.href =
+`mailto:${email}?subject=${subject}&body=${body}`;
+
+
+
+
+});
+
+
+
+
 document.getElementById("colo").onclick = () => {
 	window.location.href = "Colosseum.html";
 };
